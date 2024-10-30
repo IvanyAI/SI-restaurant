2106e5506e076e8ef396e981d63d66e15d382193
IvanyAI <adiivaniyusuf@gmail.com>
2024-10-30 20:46:46 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin Rumah Makan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  </head>
+  <body>
+    <!--navbar  -->
+    <?php include "header.php";?>
+<!--navbar  -->
+<!--CONTAINER  -->
+<div class="container-lg">
+  <div class="row">
+    <!-- sidebar -->
+    <?php include "sidebar.php";?>
+<!-- sidebar -->
+ <!--CONTAINER  -->
+ <div class="col-lg-9 mt-3">
+ <div class="card">
+  <div class="card-header">
+    Halaman Utama
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Ini adalah bagian Halaman Utama</h5>
+    <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, quasi non? Minus, iure. Magnam repellendus, perferendis dolorem consequuntur quibusdam vero earum sint dignissimos aspernatur saepe recusandae quasi quod, ratione fuga!</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+ </div>
+ <!--CONTAINER  -->
+</div>
+<div class="fixed-bottom text-center mb-2">
+  Copyright 2024 Ivany
+</div>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,34 @@
+<div class="col-lg-3">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary rounded-3 border mt-3">
+        <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" style="width:250px">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Pilihan</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav nav-pills flex-column justify-content-end flex-grow-1 ">
+          <li class="nav-item">
+            <a class="nav-link active link-light ps-2" aria-current="page" href="index.php"><i class="bi bi-kanban"></i> Halaman Utama</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link-dark ps-2" href="menu.php"><i class="bi bi-list"></i> Daftar Menu</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link-dark ps-2" href="kategori.php"><i class="bi bi-card-list"></i> Kategori Menu</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link-dark ps-2" href="akun.php"><i class="bi bi-person-badge"></i> Akun</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link-dark ps-2" href="laporan.php"><i class="bi bi-bar-chart"></i> Laporan</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+</div>
